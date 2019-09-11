84f9f559521f2241c9b94906ebbab66b9cbcc569
Skaruts <cedomingos@gmail.com>
2019-09-11 20:23:49 +0100

    Improve progress bar

--- scripts/core/Main.gd
+++ scripts/core/Main.gd
@@ -1,18 +1,4 @@
 extends Node
 
-
-
-var pb
-
 func _ready() -> void:
-							      #x, y,  w,  h, val, max_val, dir, style_override
-	pb = ui.new_progress_bar(self, 2, 2, 10,  10, 100,     100, 'd', {
-		fg = Colors.RED4,
-		bg = Color.black,
-	})
-
-func _input(event: InputEvent) -> void:
-	if Input.is_action_pressed("ui_up"):
-		pb.value += 1
-	elif Input.is_action_pressed("ui_down"):
-		pb.value -= 1
+	pass
--- scripts/ui/SK_ProgressBar.gd
+++ scripts/ui/SK_ProgressBar.gd
@@ -25,33 +25,39 @@ func _init(x:int, y:int, w:int, h:int, val:int, max_val:int, dir='right', style_
 
 func draw():
 	if visible:
-		var fill:int = int( float(value) / max_value * w )
-		var remainder:float = float(value) / max_value * w - fill
-
 		match facing:
 			Directions.UP:
+				var fill:int = int( float(value) / max_value * h )
+				var remainder:float = float(value) / max_value * h - fill
 				var length:int = h-1-fill
+
 				for j in range(h):
 					for i in range(w):
 						if j > length:     terminal.put(x+i, y+j, glyph, style.fg, style.bg)
 						elif j == length:  terminal.put(x+i, y+j, glyph, style.fg * remainder, style.bg)
 
 			Directions.LEFT:
-				var length:int = h-1-fill
+				var fill:int = int( float(value) / max_value * w )
+				var remainder:float = float(value) / max_value * w - fill
+				var length:int = w-1-fill
 				for j in range(h):
 					for i in range(w):
 						if i > length:     terminal.put(x+i, y+j, glyph, style.fg, style.bg)
 						elif i == length:  terminal.put(x+i, y+j, glyph, style.fg * remainder, style.bg)
 
 			Directions.DOWN:
+				var fill:int = int( float(value) / max_value * h )
+				var remainder:float = float(value) / max_value * h - fill
 				var length:int = fill
 				for j in range(h):
 					for i in range(w):
 						if j < length:     terminal.put(x+i, y+j, glyph, style.fg, style.bg)
 						elif j == length:  terminal.put(x+i, y+j, glyph, style.fg * remainder, style.bg)
-						else:              break
+#						else:              break
 
 			Directions.RIGHT:
+				var fill:int = int( float(value) / max_value * w )
+				var remainder:float = float(value) / max_value * w - fill
 				var length:int = fill
 				for j in range(h):
 					for i in range(w):
